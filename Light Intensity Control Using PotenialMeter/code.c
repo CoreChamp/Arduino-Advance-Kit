96a0b1dc3f16421b1147fbef2e5ab057cfcefce2
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 16:46:48 +0530

    Update code.c

--- Light Intensity Control Using PotenialMeter/code.c
+++ Light Intensity Control Using PotenialMeter/code.c
@@ -1,1 +1,12 @@
-
+#define LED_PIN 9
+#define POTENTIOMETER_PIN A4
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+}
+void loop()
+{
+  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+  int brightness = potentiometerValue / 4;
+  analogWrite(LED_PIN, brightness);
+}
