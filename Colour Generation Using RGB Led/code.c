dc2a99594bbe3a1193b5e440641a0f29b6173d95
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 17:13:19 +0530

    Update code.c

--- Colour Generation Using RGB Led/code.c
+++ Colour Generation Using RGB Led/code.c
@@ -1,1 +1,37 @@
+int red = 10;
+int green = 8;
+int blue = 9;
+void setup()
+{
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(blue,OUTPUT);
+}
 
+void loop()
+{
+  digitalWrite(red,HIGH);
+  digitalWrite(green,LOW);
+  digitalWrite(blue,LOW);
+  delay(1000);
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,LOW);
+  delay(1000);
+  digitalWrite(red,LOW);
+  digitalWrite(green,LOW);
+  digitalWrite(blue,HIGH);
+  delay(1000);
+  digitalWrite(red,HIGH);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,LOW);
+  delay(1000);
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,HIGH);
+  delay(1000);
+  digitalWrite(red,HIGH);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,HIGH);
+  delay(1000);
+}
