4b45237a25df3112ab188047730c4b94ef9e3cf5
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 17:34:37 +0530

    Update code.c

--- Led Control Using Toggle Switch/code.c
+++ Led Control Using Toggle Switch/code.c
@@ -1,1 +1,20 @@
+int led = 10;
+int toggleSwitch = 6;
+int value =0;
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(toggleSwitch,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(toggleSwitch);
+  if(value == HIGH){
+  digitalWrite(led,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(led,LOW);
+  delay(10);
+  }  
+}
