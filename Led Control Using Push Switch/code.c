ae7736bd2464a888bd6f8730efcfa288652044fa
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 17:23:48 +0530

    Update code.c

--- Led Control Using Push Switch/code.c
+++ Led Control Using Push Switch/code.c
@@ -1,1 +1,20 @@
+int led = 10;
+int button = 8;
+int value =0;
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(button,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(6);
+  if(value == HIGH){
+  digitalWrite(led,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(led,LOW);
+  delay(10);
+  }  
+}
