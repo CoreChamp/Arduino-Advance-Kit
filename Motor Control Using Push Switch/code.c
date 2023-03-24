1db9118f837d1db596395ed19d0a9cc22b4c2655
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 17:48:03 +0530

    Update code.c

--- Motor Control Using Push Switch/code.c
+++ Motor Control Using Push Switch/code.c
@@ -1,1 +1,21 @@
+int motor = 10;
+int button = 8;
+int value =0;
+void setup()
+{
+  pinMode(motor,OUTPUT);
+  pinMode(button,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(6);
+  if(value == HIGH){
+  digitalWrite(motor,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(motor,LOW);
+  delay(10);
+  }
+  
+}
