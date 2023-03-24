de1a46e57006596b30b0f05f16f668867584e031
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-24 17:52:57 +0530

    Update code.c

--- Motor Control Using Toggle Switch/code.c
+++ Motor Control Using Toggle Switch/code.c
@@ -1,1 +1,20 @@
+int motor = 10;
+int toggleSwitch = 6;
+int value =0;
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(toggleSwitch,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(toggleSwitch);
+  if(value == HIGH){
+  digitalWrite(motor,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(motor,LOW);
+  delay(10);
+  }  
+}
