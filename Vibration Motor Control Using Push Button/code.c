04dedee95e5f1c447e4915ed88a4bfae3a8ca876
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-25 13:18:48 +0530

    Update code.c

--- Vibration Motor Control Using Push Button/code.c
+++ Vibration Motor Control Using Push Button/code.c
@@ -1,1 +1,21 @@
+int vibmotor = 10;
+int button = 8;
+int value = 0;
+void setup()
+{
+  pinMode(motor,OUTPUT);
+  pinMode(button,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(button);
+  if(value == HIGH){
+  digitalWrite(vibmotor,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(vibmotor,LOW);
+  delay(10);
+  }
+  
+}
