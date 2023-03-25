a4df42eb220626eb6b13979ad1fba63c1135d23d
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-25 13:42:56 +0530

    Update code.c

--- Vibration Motor Control Using Touch Sensor/code.c
+++ Vibration Motor Control Using Touch Sensor/code.c
@@ -1,1 +1,29 @@
+const int TOUCH_SENSOR_PIN  = 4; 
+const int vibmotor_PIN = 2; 
 
+int vibmotorState = LOW;    
+int lastTouchState;   
+int currentTouchState; 
+
+void setup() {
+  Serial.begin(9600);               
+  pinMode(TOUCH_SENSOR_PIN, INPUT); 
+  pinMode(vibmotor_PIN, OUTPUT);         
+
+  currentTouchState = digitalRead(TOUCH_SENSOR_PIN);
+}
+
+void loop() {
+  lastTouchState    = currentTouchState;             // save the last state
+  currentTouchState = digitalRead(TOUCH_SENSOR_PIN); // read new state
+
+  if(lastTouchState == LOW && currentTouchState == HIGH) {
+    Serial.println("The sensor is touched");
+
+   
+    vibmotorState = !vibmotorState;
+
+    
+    digitalWrite(vibmotor_PIN, vibmotorState); 
+  }
+}
