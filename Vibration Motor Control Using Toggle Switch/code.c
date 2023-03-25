559e2f17c6f452cc3a79bb360685937e1c194668
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-25 13:35:39 +0530

    Update code.c

--- Vibration Motor Control Using Toggle Switch/code.c
+++ Vibration Motor Control Using Toggle Switch/code.c
@@ -1,1 +1,20 @@
+int vibmotor = 10;
+int toggleSwitch = 6;
+int value =0;
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(toggleSwitch,INPUT);
+}
 
+void loop()
+{
+  value = digitalRead(toggleSwitch);
+  if(value == HIGH){
+  digitalWrite(vibmotor,HIGH);
+  delay(10);
+  }else{
+  digitalWrite(vibmotor,LOW);
+  delay(10);
+  }  
+}
