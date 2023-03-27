f54d88f6025f3f2fbc9facd620b92fe2fed39c11
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-27 14:11:08 +0530

    Update code.c

--- Smoke Detection using Smoke Sensor/code.c
+++ Smoke Detection using Smoke Sensor/code.c
@@ -1,10 +1,13 @@
+
 int smokeA0 = A0;
+int led = 6;
 // Your threshold value
 int sensorThres = 100;
  
 void setup() {
 
   pinMode(smokeA0, INPUT);
+  pinMode(led.HIGH);
   Serial.begin(9600);
 }
  
@@ -17,10 +20,13 @@ void loop() {
   if (analogSensor > sensorThres)
   {
     Serial.println("Smoke Detected!");
+    digitalWrite(led,HIGH);
   }
   else
   {
-    Serial.println("Normal");    
+    Serial.println("Normal");
+    digitalWrite(led,LOW);   
   }
   delay(500);
+
 }
