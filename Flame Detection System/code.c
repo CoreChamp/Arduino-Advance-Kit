1f5f9b1448f20f696b31646fd0644f3e147bec88
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-27 11:05:45 +0530

    Create code.c

---
+++ Flame Detection System/code.c
@@ -0,0 +1,27 @@
+int sensorvalue = 0; 
+int buzzer = 6;
+
+void setup() {
+  pinMode(8,INPUT);
+  Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  sensorvalue = digitalRead(8);
+  Serial.print("Sensor value =");
+  Serial.print(sensorvalue);
+  Serial.print('\n');
+  delay(1000);
+  
+  if(sensorvalue == 1){
+  Serial.println("Warning FIRE");
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  }
+  else{
+    Serial.println("No Flames");
+    digitalWrite(buzzer,LOW);
+      delay(100);
+  }
+}
