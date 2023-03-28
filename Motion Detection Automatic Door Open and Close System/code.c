c4ba7b3ac3d24d1363a769e9d4f36e9c720312dd
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-28 10:27:03 +0530

    Update code.c

--- Motion Detection Automatic Door Open and Close System/code.c
+++ Motion Detection Automatic Door Open and Close System/code.c
@@ -10,7 +10,7 @@ void setup()
 {
   myservo.attach(9);
   pinMode(12, INPUT); 
-  pinMode(13,OUTPUT);
+  pinMode(led,OUTPUT);
   Serial.begin(9600);
   
 }
@@ -20,12 +20,12 @@ void loop()
   sensorState = digitalRead(12);
    
   if (sensorState == HIGH) {
-    digitalWrite(13, HIGH);
+    digitalWrite(led, HIGH);
     Serial.println("Door Opening");    
     myservo.write(180);                       
   
   } else {
-    digitalWrite(13, LOW);
+    digitalWrite(led, LOW);
     Serial.println("Door Closing");
     myservo.write(0);
   }
