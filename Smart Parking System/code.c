b827b4315abee385fbe1f16ac8a701ef7b31c380
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-05-22 16:09:40 +0530

    Update code.c

--- Smart Parking System/code.c
+++ Smart Parking System/code.c
@@ -22,33 +22,39 @@ lcd.setCursor (0,0);
 lcd.print("     ARDUINO    ");
 lcd.setCursor (0,1);
 lcd.print(" PARKING SYSTEM ");
+
 delay (2000);
 lcd.clear();  
 }
 
 void loop(){ 
 
-if(digitalRead (IR1) == LOW && flag1==0){
-if(Slot>0){flag1=1;
-if(flag2==0){myservo1.write(0); Slot = Slot-1;}
-}else{
+if(digitalRead (IR1) == LOW){
+if(Slot>0){
+  myservo1.write(0); 
+  Slot = Slot-1;
+  delay(1000);
+}
+else{
 lcd.setCursor (0,0);
 lcd.print("    SORRY :(    ");  
 lcd.setCursor (0,1);
 lcd.print("  Parking Full  "); 
+Slot = 0;
 delay (3000);
 lcd.clear(); 
 }
 }
 
-if(digitalRead (IR2) == LOW && flag2==0){flag2=1;
-if(flag1==0){myservo1.write(0); Slot = Slot+1;}
+if(digitalRead (IR2) == LOW ){
+  myservo1.write(0); 
+  Slot = Slot+1;
+  delay(100);
 }
 
-if(flag1==1 && flag2==1){
-delay (1000);
+if(digitalRead (IR1) == HIGH && digitalRead (IR2) == HIGH){
+delay (100);
 myservo1.write(100);
-flag1=0, flag2=0;
 }
 
 lcd.setCursor (0,0);
@@ -56,4 +62,8 @@ lcd.print("    WELCOME!    ");
 lcd.setCursor (0,1);
 lcd.print("Slot Left: ");
 lcd.print(Slot);
+delay (1000);
 }
+
+
+
